f22981a752c17af0cc71cd21f593ce460ddf8b19
Ivan Mang <ivanmang1997@gmail.com>
2021-02-06 10:11:26 +0800

    Return TODO

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -7,7 +7,6 @@ import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
 
     private val reminders : MutableList<ReminderDTO> = mutableListOf()
 
